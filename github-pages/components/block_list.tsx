638643c0644c831874d6a7506c2db96af9b158fe
Gabriele Nunez <gabrielenunez@protonmail.com>
2023-08-04 05:06:17 -0400

    Moved background image into its own inner div with its own css class properties for the more complex styles

--- github-pages/components/block_list.tsx
+++ github-pages/components/block_list.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { H3 } from './elements/headings';
 import Button from './elements/button';
 
@@ -14,31 +14,40 @@ export interface BlockListProps {
   className?: string;
 }
 
-export const BlockList = (props: BlockListProps) => (
-  <div className={'flex flex-wrap ' + (props.className || '')}>
-    {props.items.map((item, itemIndex) => (
-      <div
-        title={item.caption}
-        className="relative top-0 group flex-1 basis-2/4 flex justify-center items-center  min-h-[12.5rem] block-list-item text-center flex-wrap"
-        key={props.id + '_' + itemIndex}
-        style={
-          {
-            '--background': "url('" + (item.url_background || '') + "')",
-          } as React.CSSProperties
-        }
-      >
-        <H3 className=" z-10 basis-full lg:group-hover:opacity-0 opacity:100  ease-in-out duration-300 ">
-          {item.text}
-        </H3>
-        <Button
-          className=" z-10 absolute max-w-[10rem] lg:group-hover:opacity-100 opacity-100 lg:opacity-0 bottom-8 md:bottom-12  lg:bottom-auto"
-          intention={'normal'}
+export const BlockList = (props: BlockListProps) => {
+  return (
+    <div className={'flex flex-wrap ' + (props.className || '')}>
+      {props.items.map((item, itemIndex) => (
+        <div
+          title={item.caption}
+          className=" relative top-0 group flex-1 basis-2/4 flex justify-center items-center  min-h-[12.5rem] block-list-item text-center flex-wrap overflow-hidden"
+          key={props.id + '_' + itemIndex}
         >
-          Learn More
-        </Button>
-      </div>
-    ))}
-  </div>
-);
+          {item.url_background ? (
+            <div
+              className="z-0 block-list-item-image"
+              style={
+                {
+                  '--background': "url('" + (item.url_background || '') + "')",
+                } as React.CSSProperties
+              }
+            />
+          ) : (
+            <></>
+          )}
+          <H3 className=" z-10 basis-full lg:group-hover:opacity-0 opacity:100  ease-in-out duration-300 ">
+            {item.text}
+          </H3>
+          <Button
+            className=" absolute z-10 max-w-[10rem] lg:group-hover:opacity-100 opacity-100 lg:opacity-0 bottom-8 md:bottom-12  lg:bottom-auto"
+            intention={'normal'}
+          >
+            Learn More
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default BlockList;
